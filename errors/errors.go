fb09bcb3888f4d62cdab725e587d19dabf958722
Rob Bridges <Rmb101187@gmail.com>
2023-09-17 09:59:19 -0700

    Removed unused error code

--- errors/errors.go
+++ errors/errors.go
@@ -2,7 +2,6 @@ package errors
 
 const (
 	InvalidUser          = "User password must be 4 characters long and email must be 5 characters long"
-	InvalidPassword      = "user password must be greater than 4 characters and less than 72"
 	InternalServerError  = "Internal Server Error"
 	InvalidCredentials   = "Invalid Credentials"
 	PasswordResetEmail   = "Password reset email sent, please check your inbox"
