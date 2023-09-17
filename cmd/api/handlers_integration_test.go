8f5df0f16d664ee61519b47a71539617b7ca59a2
Rob Bridges <Rmb101187@gmail.com>
2023-09-17 11:30:00 -0700

    Deleted wrong code, now fixed

--- cmd/api/handlers_integration_test.go
+++ cmd/api/handlers_integration_test.go
@@ -103,122 +103,6 @@ func TestApp_CreateUserIntegration(t *testing.T) {
 		// delete user to keep test db clean
 		app.userModel.DeleteUser(user.Email)
 	})
-	t.Run("Duplicate user", func(t *testing.T) {
-		server := httptest.NewServer(http.HandlerFunc(app.CreateUser))
-		defer server.Close()
-
-		req, err := http.NewRequest("POST", server.URL+"/users", bytes.NewBuffer(payload))
-		if err != nil {
-			t.Fatal(err)
-		}
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			t.Errorf("Expected status %d, but got %d", http.StatusOK, resp.StatusCode)
-		}
-
-		//parse user
-		var user models.User
-		err = json.NewDecoder(resp.Body).Decode(&user)
-		if err != nil {
-			t.Errorf("Error unmarshaling JSON: %v", err)
-		}
-		err = app.userModel.Insert(&user)
-		if err == nil {
-			t.Errorf("Expected error, but got nil")
-		}
-		// try to insert the same user with the same payload, expect error
-		resp, err = http.DefaultClient.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Errorf("Unexpected error reading response body: %v", err)
-		}
-
-		if string(body) != "duplicate email\n" {
-			t.Errorf("Expected body %s, but got %s", "duplicate email\n", string(body))
-		}
-		app.userModel.DeleteUser(user.Email)
-	})
-	t.Run("Invalid user", func(t *testing.T) {
-		server := httptest.NewServer(http.HandlerFunc(app.CreateUser))
-		defer server.Close()
-
-		// init request
-		req, err := http.NewRequest("POST", server.URL+"/users", bytes.NewBuffer(badEmailPayload))
-		if err != nil {
-			t.Errorf("Unexpected error in get request to %s", req.URL)
-		}
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusBadRequest {
-			t.Errorf("Expected status %d, but got %d", http.StatusBadRequest, resp.StatusCode)
-		}
-		// parse user from response body should be good json, but user should not validate
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Errorf("Unexpected error reading response body: %v", err)
-		}
-
-		// validator should not be happy with the bad payload
-		if string(body) != "User password must be 4 characters long and email must be 5 characters long\n" {
-			t.Errorf("Expected body %s, but got %s", "invalid user\n", string(body))
-		}
-
-	})
-}
-
-func TestApp_GetUserIntegration(t *testing.T) {
-	testCfg := data.TestPostgresConfig()
-	testDB, err := data.Open(testCfg)
-	defer testDB.Close()
-	if err != nil {
-		t.Errorf("Expected database to open, but got %s", err)
-	}
-	app := App{userModel: &models.UserModel{DB: testDB}}
-	t.Run("Get user Happy path", func(t *testing.T) {
-
-		server := httptest.NewServer(http.HandlerFunc(app.getUserByEmail))
-		defer server.Close()
-		payloadEmailOnly := []byte(`{"email": "admin@localhost"}`)
-		req, err := http.NewRequest("GET", server.URL+"/users)", bytes.NewBuffer(payloadEmailOnly))
-		if err != nil {
-			t.Errorf("Unexpected error in get request to %s", req.URL)
-		}
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			t.Errorf("Expected status %d, but got %d", http.StatusOK, resp.StatusCode)
-		}
-
-		var userReturned models.User
-		err = json.NewDecoder(resp.Body).Decode(&userReturned)
-		if err != nil {
-			t.Errorf("Error unmarshaling JSON: %v", err)
-		}
-		t.Log(userReturned)
-		var expectedUser = models.User{
-			ID:       1,
-			Email:    "admin@localhost",
-			Password: "$2a$10$m2RvoCSnhAMGZggN1SPPsOwlSC8Ne0EX.wi7EHK2/pKKmoOmDQsUe",
-		}
-		if reflect.DeepEqual(userReturned, expectedUser) {
-			t.Errorf("Expected user %v, but got %v", expectedUser, userReturned)
-		}
-	})
 }
 
 func TestApp_CreateUserIntegration_SadPaths(t *testing.T) {
@@ -324,6 +208,103 @@ func TestApp_CreateUserIntegration_SadPaths(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestApp_GetUserIntegration(t *testing.T) {
+	testCfg := data.TestPostgresConfig()
+	testDB, err := data.Open(testCfg)
+	defer testDB.Close()
+	if err != nil {
+		t.Errorf("Expected database to open, but got %s", err)
+	}
+	app := App{userModel: &models.UserModel{DB: testDB}}
+	t.Run("Get user Happy path", func(t *testing.T) {
+
+		server := httptest.NewServer(http.HandlerFunc(app.getUserByEmail))
+		defer server.Close()
+		payloadEmailOnly := []byte(`{"email": "admin@localhost"}`)
+		req, err := http.NewRequest("GET", server.URL+"/users)", bytes.NewBuffer(payloadEmailOnly))
+		if err != nil {
+			t.Errorf("Unexpected error in get request to %s", req.URL)
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status %d, but got %d", http.StatusOK, resp.StatusCode)
+		}
+
+		var userReturned models.User
+		err = json.NewDecoder(resp.Body).Decode(&userReturned)
+		if err != nil {
+			t.Errorf("Error unmarshaling JSON: %v", err)
+		}
+		t.Log(userReturned)
+		var expectedUser = models.User{
+			ID:       1,
+			Email:    "admin@localhost",
+			Password: "$2a$10$m2RvoCSnhAMGZggN1SPPsOwlSC8Ne0EX.wi7EHK2/pKKmoOmDQsUe",
+		}
+		if reflect.DeepEqual(userReturned, expectedUser) {
+			t.Errorf("Expected user %v, but got %v", expectedUser, userReturned)
+		}
+	})
+
+	t.Run("Get user Sad path", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(app.getUserByEmail))
+		defer server.Close()
+		payload := []byte(`{"email": "adminx@localhost"}`)
+		req, err := http.NewRequest("GET", server.URL+"/users)", bytes.NewBuffer(payload))
+		if err != nil {
+			t.Errorf("Unexpected error in get request to %s", req.URL)
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected status %d, but got %d", http.StatusBadRequest, resp.StatusCode)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("Unexpected error reading response body: %v", err)
+		}
+
+		expectedBody := "record not found\n"
+
+		if string(body) != expectedBody {
+			t.Errorf("Expected body %s, but got %s", expectedBody, string(body))
+		}
+
+	})
+	t.Run("Get user Invalid email payload", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(app.getUserByEmail))
+		defer server.Close()
+
+		req, err := http.NewRequest("GET", server.URL+"/users", bytes.NewBuffer(emailOnlyBadPayload))
+		if err != nil {
+			t.Errorf("Unexpected error in get request to %s", req.URL)
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected status %d, but got %d", http.StatusBadRequest, resp.StatusCode)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("Unexpected error reading response body: %v", err)
+		}
+
+		if string(body) != "User password must be 4 characters long and email must be 5 characters long\n" {
+			t.Errorf("Expected body %s, but got %s", "invalid user\n", string(body))
+		}
+	})
 }
 
 func Test_UpdatePasswordIntegration(t *testing.T) {
