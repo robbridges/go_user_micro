ee463a21d8358e7a0daa8501b21c173af330186c
Rob Bridges <Rmb101187@gmail.com>
2023-09-20 20:50:04 -0700

    Added missing test for opening db by dsn

--- data/postgres_integration_test.go
+++ data/postgres_integration_test.go
@@ -8,7 +8,7 @@ func TestOpen(t *testing.T) {
 
 	cfg := DefaultPostgresConfig()
 	db, err := Open(cfg)
-	
+
 	if err != nil {
 		t.Errorf("Expected no error, but got %s", err)
 	}
@@ -16,4 +16,15 @@ func TestOpen(t *testing.T) {
 		t.Errorf("Expected a DB connection, but got nil")
 	}
 
+}
+
+func TestOpenDSN(t *testing.T) {
+
+	db, err := OpenDSN("DSN_DB")
+	if err != nil {
+		t.Errorf("Expected no error, but got %s", err)
+	}
+	if db == nil {
+		t.Errorf("Expected a DB connection, but got nil")
+	}
 }
